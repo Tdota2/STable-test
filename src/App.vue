043d5ee2e6d7c4076ccf7cae3dd69c7ab3bb5f31
滕琦 <2552776891@qq.com>
2024-04-15 11:09:35 +0800

    测试demo：rowHeight设置失效问题

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,91 @@
 <template>
   <s-table
     :columns="columns"
-    :scroll="{ y: 400 }"
-    :pagination="false"
     :data-source="dataSource"
-    :row-selection="rowSelection"
-    default-expand-all-rows
+    :rowHeight="() => {
+            return 80
+      }"
   ></s-table>
 </template>
-<script lang="ts" setup>
-import { STableProps } from "@surely-vue/table";
-import { ref } from "vue";
-const columns = [
-  {
-    title: "Full Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-  },
-];
-const dataSource = ref([
-  {
-    key: 0,
-    name: "001",
-    age: 1,
-    children: [
-      {
-        key: 1,
-        name: "001-1",
-        age: 2,
-      },
-    ],
-  },
-  { key: 2, name: "002", age: 3 },
-]);
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
 
-const rowSelection: STableProps["rowSelection"] = {
-  checkStrictly: false,
-  getCheckboxProps: (record: any) => ({
-    disabled: record.key === 1, // Column configuration not to be checked
-  }),
-};
+interface DataItem {
+  key: string;
+  name: string;
+  money: string;
+  address: string;
+  children?: Array<DataItem>;
+}
+
+export default defineComponent({
+  setup() {
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        autoHeight: true,
+      },
+      {
+        title: 'Cash Assets',
+        dataIndex: 'money',
+        align: 'right',
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+      },
+    ];
+    const data: DataItem[] = [
+      {
+        key: '1',
+        name: 'John Brown',
+        money: '￥300,000.00',
+        address: 'New York No. 1 Lake Park',
+        children: [
+          {
+            key: '11',
+            name: 'John Brown1',
+            money: '￥3400,000.00',
+            address: 'New York No. 11 Lake Park',
+          },
+        ],
+      },
+      {
+        key: '2',
+        name: 'Jim Green',
+        money: '￥1,256,000.00',
+        address: 'London No. 1 Lake Park',
+      },
+      {
+        key: '3',
+        name: 'Joe Black',
+        money: '￥120,000.00',
+        address: 'Sidney No. 1 Lake Park',
+      },
+      {
+        key: '4',
+        name: 'Tom',
+        money: '￥186,000.00',
+        address: 'Sidney No. 2 Lake Park',
+      },
+      {
+        key: '5',
+        name: 'Tony',
+        money: '￥110,000.00',
+        address: 'Sidney No. 2 Lake Park',
+      },
+      {
+        key: '6',
+        name: 'YaYa',
+        money: '￥200,000.00',
+        address: 'Sidney No. 4 Lake Park',
+      },
+    ];
+    return {
+      dataSource: data,
+      columns: ref(columns),
+    };
+  },
+});
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import STable from "@surely-vue/table";
 import App from "./App.vue";
 import Antdv from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.less';
 import "@surely-vue/table/dist/index.less";
 
 const app = createApp(App);
