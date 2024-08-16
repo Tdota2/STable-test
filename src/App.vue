b92e2b2afbb48a96940e3b0b74285c2a97c4f304
滕琦 <2552776891@qq.com>
2024-08-16 16:54:06 +0800

    修改示例：复现初始化表格，加载数据后触发change事件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
-  <s-table
-    :columns="columns"
-    :data-source="dataSource"
-    :rowHeight="() => {
-            return 80
-      }"
-  ></s-table>
+  <s-table v-model:pagination="pagination" :columns="columns" :data-source="data" :height="400" @change="change">
+    <template #bodyCell="{ column, text, record }">
+      <template v-if="column.key === 'action'">
+        <a-space size="middle">
+          <a>Invite {{ record.name }}</a>
+          <a>Delete</a>
+        </a-space>
+      </template>
+      <template v-else-if="column.key === 'name'">
+        <a>{{ text }}</a>
+      </template>
+      <template v-else-if="column.key === 'tags'">
+        <span>
+          <a-tag
+            v-for="tag in text"
+            :key="tag"
+            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+          >
+            {{ tag.toUpperCase() }}
+          </a-tag>
+        </span>
+      </template>
+    </template>
+  </s-table>
 </template>
 <script lang="ts">
+import type { STablePaginationConfig } from '@surely-vue/table';
 import { defineComponent, ref } from 'vue';
 
 interface DataItem {
   key: string;
   name: string;
-  money: string;
+  age: number;
   address: string;
-  children?: Array<DataItem>;
+  tags: string[];
 }
 
 export default defineComponent({
@@ -24,67 +42,86 @@ export default defineComponent({
       {
         title: 'Name',
         dataIndex: 'name',
-        autoHeight: true,
+        key: 'name',
       },
       {
-        title: 'Cash Assets',
-        dataIndex: 'money',
-        align: 'right',
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
       },
       {
         title: 'Address',
         dataIndex: 'address',
+        key: 'address',
+      },
+      {
+        title: 'Tags',
+        key: 'tags',
+        dataIndex: 'tags',
+      },
+      {
+        title: 'Action',
+        key: 'action',
       },
     ];
-    const data: DataItem[] = [
-      {
-        key: '1',
-        name: 'John Brown',
-        money: '￥300,000.00',
-        address: 'New York No. 1 Lake Park',
-        children: [
-          {
-            key: '11',
-            name: 'John Brown1',
-            money: '￥3400,000.00',
-            address: 'New York No. 11 Lake Park',
-          },
-        ],
-      },
-      {
-        key: '2',
-        name: 'Jim Green',
-        money: '￥1,256,000.00',
-        address: 'London No. 1 Lake Park',
-      },
-      {
-        key: '3',
-        name: 'Joe Black',
-        money: '￥120,000.00',
-        address: 'Sidney No. 1 Lake Park',
-      },
-      {
-        key: '4',
-        name: 'Tom',
-        money: '￥186,000.00',
-        address: 'Sidney No. 2 Lake Park',
-      },
-      {
-        key: '5',
-        name: 'Tony',
-        money: '￥110,000.00',
-        address: 'Sidney No. 2 Lake Park',
-      },
-      {
-        key: '6',
-        name: 'YaYa',
-        money: '￥200,000.00',
-        address: 'Sidney No. 4 Lake Park',
-      },
-    ];
+    const data = ref<DataItem[]>([]);
+
+    const pagination = ref<STablePaginationConfig>({
+      showQuickJumper: true,
+      showSizeChanger: true,
+      showTotal: (total) => `Total ${total} items`,
+      pageSize: 20,
+    });
+
+    const change = () => {
+      console.log(1111111);
+    };
+
+    const init = () => {
+      data.value = [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ];
+      for (let i = 0; i < 1000; i++) {
+        const num = data.value.length + 1;
+        data.value.push({
+          key: String(num),
+          name: `John Brown ${num}`,
+          age: i + 1,
+          address: `London Park no. ${num}`,
+          tags: ['cool', 'teacher', 'loser'],
+        });
+      }
+      console.log('init111111');
+    };
+
+    onMounted(() => {
+      init();
+    });
     return {
-      dataSource: data,
+      data,
       columns: ref(columns),
+      pagination,
+      change,
     };
   },
 });
